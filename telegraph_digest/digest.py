89075e5ba94ba012d852f3bd0c7830f0fc11845e
FIL <git@fillll.ru>
2017-03-04 17:17:33 +0000

    PORABOTAL ESHE S @FEDUZA

--- telegraph_digest/digest.py
+++ telegraph_digest/digest.py
@@ -18,16 +18,17 @@ def normalization_coef(data):
 def good_stufff(subs, reddit):
     submissons_with_cross_scores = dict()
     for sub in subs:
-        submissons = [s for s in reddit.subreddit(sub).hot(limit=HOT_LIMIT)]
+        submissons = [s for s in reddit.subreddit(sub).hot(limit=(HOT_LIMIT * 2))]
         scores = [s.score for s in submissons]
         scores = [s * normalization_coef(scores) for s in scores]
         for ind, submisson in enumerate(submissons):
             submissons_with_cross_scores[submisson.id] = {
                 'self': submisson,
-                'cross_score': scores[ind]
+                'cross_score': scores[ind],
+                'img_data': utils.do_magic(submisson)
             }
-    return sorted(submissons_with_cross_scores.items(), key=lambda x: x[1]['cross_score'], reverse=True)[:HOT_LIMIT]
-
+    img_and_gifs = [i for i in submissons_with_cross_scores.items() if i[1]['img_data']['type'] in [utils.TYPE_GIF, utils.TYPE_IMG]]
+    return dict(sorted(img_and_gifs, key=lambda x: x[1]['cross_score'], reverse=True)[:HOT_LIMIT])
 
 
 def supply(sub, config):
@@ -50,4 +51,4 @@ if __name__ == '__main__':
     parser.add_argument('--config', default='prod.yml')
     # parser.add_argument('--sub')
     args = parser.parse_args()
-    main(args.config, 'boobs')
+    pprint(main(args.config, 'boobs'))
--- telegraph_digest/utils.py
+++ telegraph_digest/utils.py
@@ -2,23 +2,11 @@
 
 from urllib.parse import urlparse
 import requests
-from requests.exceptions import InvalidSchema
 import os
 import imghdr
-import time
-import random
-import re
-import hashlib
-from datetime import datetime
-import logging
 
 from imgurpython import ImgurClient
 import yaml
-import pymongo
-import telepot
-
-
-TELEGRAM_AUTOPLAY_LIMIT = 10 * 1024 * 1024
 
 
 TYPE_IMG = 'img'
@@ -35,7 +23,16 @@ TYPE_ALBUM = 'album'
 TEMP_FOLDER = 'tmp'
 
 
-def get_url(submission, mp4_instead_gif=True):
+def do_magic(submission):
+    stuff = get_url(submission)
+    return {
+        'type': stuff[0],
+        'url': stuff[1],
+        'extension': stuff[2]
+    }
+
+
+def get_url(submission, mp4_instead_gif=False):
     '''
     return TYPE, URL, EXTENSION
     E.x.: return 'img', 'http://example.com/pic.png', 'png'
@@ -76,8 +73,8 @@ def get_url(submission, mp4_instead_gif=True):
 
     if urlparse(url).netloc == 'imgur.com':
         # Imgur
-        imgur_config = yaml.load(open('imgur.yml').read())
-        imgur_client = ImgurClient(imgur_config['client_id'], imgur_config['client_secret'])
+        imgur_config = yaml.load(open('prod.yml').read())
+        imgur_client = ImgurClient(imgur_config['imgur']['client_id'], imgur_config['imgur']['client_secret'])
         path_parts = urlparse(url).path.split('/')
         if path_parts[1] == 'gallery':
             # TODO: gallary handling
@@ -119,203 +116,3 @@ def get_url(submission, mp4_instead_gif=True):
                     return TYPE_GIF, img.gifv[:-1], 'gif'
     else:
         return TYPE_OTHER, url, None
-
-
-def download_file(url, filename):
-    # http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-    chunk_counter = 0
-    with open(filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-                #f.flush() commented by recommendation from J.F.Sebastian
-                chunk_counter += 1
-                # It is not possible to send greater than 50 MB via Telegram
-                if chunk_counter > 50 * 1024:
-                    return False
-    return True
-
-
-def md5_sum_from_url(url):
-    try:
-        r = requests.get(url, stream=True)
-    except InvalidSchema:
-        return None
-    except MissingSchema:
-        return None
-    chunk_counter = 0
-    hash_store = hashlib.md5()
-    for chunk in r.iter_content(chunk_size=1024):
-        if chunk:  # filter out keep-alive new chunks
-            hash_store.update(chunk)
-            chunk_counter += 1
-            # It is not possible to send greater than 50 MB via Telegram
-            if chunk_counter > 50 * 1024:
-                return None
-    return hash_store.hexdigest()
-
-
-def weighted_random_subreddit(d):
-    r = random.uniform(0, sum(val for val in d.values()))
-    s = 0.0
-    for k, w in d.items():
-        s += w
-        if r < s: return k
-    return k
-
-
-class Reddit2TelegramSender(object):
-    '''
-    docstring for reddit2telegram
-    '''
-    def __init__(self, t_channel, config):
-        super(Reddit2TelegramSender, self).__init__()
-        self.config = config
-        self.telepot_bot = telepot.Bot(self.config['telegram_token'])
-        self.t_channel = t_channel
-        self._make_mongo_connections()
-        self._store_stats()
-
-    def _make_mongo_connections(self):
-        self.stats = pymongo.MongoClient(host=self.config['db_host'])[self.config['db']]['stats']
-        self.urls = pymongo.MongoClient(host=self.config['db_host'])[self.config['db']]['urls']
-        self.contents = pymongo.MongoClient(host=self.config['db_host'])[self.config['db']]['contents']
-
-    def _store_stats(self):
-        self.stats.insert_one({
-            'channel': self.t_channel.lower(),
-            'ts': datetime.utcnow(),
-            'members_cnt': self.telepot_bot.getChatMembersCount(self.t_channel)
-        })
-
-    def _get_file_name(self, ext):
-        return os.path.join(TEMP_FOLDER,
-                            '{name}.{ext}'.format(name=self.t_channel[1:], ext=ext))
-
-    def _split_200(self, text):
-        new_text = ''
-        next_text = ''
-        list_of_words = re.split('[ \n]', text)
-        switched = False
-        for i in list_of_words:
-            if (len(new_text) + len(i) + 1 < 196) and not switched:
-                new_text += i + ' '
-            else:
-                switched = True
-                next_text += ' ' + i
-        return new_text + ' ...', '... ' + next_text
-
-    def _split_4096(self, text):
-        new_text = text[:4096]
-        next_text = text[4096:]
-        return new_text, next_text
-
-    def was_before(self, url):
-        result = self.urls.find_one({
-            'channel': self.t_channel.lower(),
-            'url': {'$regex': url.split('/')[-1]}
-        })
-        if result is None:
-            return False
-        else:
-            return True
-
-    def mark_as_was_before(self, url):
-        self.urls.insert_one({
-            'url': url,
-            'ts': datetime.utcnow(),
-            'channel': self.t_channel.lower()
-        })
-
-    def dup_check_and_mark(self, url):
-        md5_sum = md5_sum_from_url(url)
-        if md5_sum is None:
-            return False
-        result = self.contents.find_one({
-            'channel': self.t_channel.lower(),
-            'md5_sum': md5_sum
-        })
-        if result is None:
-            self.contents.insert_one({
-                'md5_sum': md5_sum,
-                'ts': datetime.utcnow(),
-                'channel': self.t_channel.lower()
-            })
-            return False
-        else:
-            return True
-
-    def send_gif_img(self, what, url, ext, text):
-        if what == TYPE_GIF:
-            return self.send_gif(url, ext, text)
-        elif what == TYPE_IMG:
-            return self.send_img(url, ext, text)
-        else:
-            return False
-
-    def send_gif(self, url, ext, text):
-        filename = self._get_file_name(ext)
-        # Download gif
-        if not download_file(url, filename):
-            return False
-        # Telegram will not autoplay big gifs
-        if os.path.getsize(filename) > TELEGRAM_AUTOPLAY_LIMIT:
-            return False
-        next_text = ''
-        if len(text) > 200:
-            text, next_text = self._split_200(text)
-        f = open(filename, 'rb')
-        self.telepot_bot.sendDocument(self.t_channel, f, caption=text)
-        f.close()
-        if len(next_text) > 1:
-            time.sleep(2)
-            self.send_text(next_text, disable_web_page_preview=True)
-        return True
-
-    def send_img(self, url, ext, text):
-        filename = self._get_file_name(ext)
-        # Download file
-        if not download_file(url, filename):
-            return False
-        next_text = ''
-        if len(text) > 200:
-            text, next_text = self._split_200(text)
-        f = open(filename, 'rb')
-        self.telepot_bot.sendPhoto(self.t_channel, f, caption=text)
-        f.close()
-        if len(next_text) > 1:
-            time.sleep(2)
-            self.send_text(next_text, disable_web_page_preview=True)
-        return True
-
-    def send_text(self, text, disable_web_page_preview=False):
-        if len(text) < 4096:
-            self.telepot_bot.sendMessage(self.t_channel, text, disable_web_page_preview=disable_web_page_preview)
-            return True
-        # If text is longer than 4096 symnols.
-        next_text = text
-        while len(next_text) > 0:
-            new_text, next_text = self._split_4096(next_text)
-            self.telepot_bot.sendMessage(self.t_channel, new_text, disable_web_page_preview=disable_web_page_preview)
-            time.sleep(2)
-        return True
-
-    def send_album(self, story):
-        def just_send(num, item):
-            text = '# {}\n{}'.format(num, item['url'])
-            self.send_text(text)
-
-        for num, item in sorted(story.items(), key=lambda x: x[0]):
-            text = '# {}'.format(num)
-            if item['what'] == TYPE_GIF:
-                if not self.send_gif(item['url'], item['ext'], text):
-                    just_send(num, item)
-            elif item['what'] == TYPE_IMG:
-                if not self.send_img(item['url'], item['ext'], text):
-                    just_send(num, item)
-            elif item['what'] == 'text':
-                just_send(num, item)
-            time.sleep(2)
-        return True
