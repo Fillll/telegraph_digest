bbed96284545e4c20df8d824064a791ce447cf7c
Fillll <git@alexfil.com>
2026-01-09 20:10:38 -0500

    Add cron log markers

--- cron_job.sh
+++ cron_job.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
+echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ") cron_job start"
 source env/bin/activate
 cd telegraph_digest
 python boobs_sender.py
+echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ") cron_job end"
